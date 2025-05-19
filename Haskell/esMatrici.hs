bdad38aa548531e65dba6f921c4e9cca7c53bfbc
riccardosantarossa <rikysanta03@gmail.com>
2025-05-19 20:24:04 +0200

    aggiunti es.4 ed es.8

--- Haskell/esMatrici.hs
+++ Haskell/esMatrici.hs
@@ -2,9 +2,32 @@
 --1. controlla se tutte le righe della matrice (lista di liste) hanno la stessa lunghezza
 --TODO
 
+-----------------------------------------------------------------
+
 --2. calcola il vettore della somma dei valori delle colonne della matrice (M memorizzata per colonne come lista di liste)
 colSums :: [[Int]] -> [Int]
 colSums xss = map sum xss
 
+-----------------------------------------------------------------
 
+--4 data M implementata per righe calcola le coppie (minimo, massimo) delle colonne della matrice
 
+--estraggo le colonne dalla matrice espressa per righe
+extractCols:: [[Int]] -> [[Int]]
+extractCols ([]:_) = []
+extractCols xss = map head xss : extractCols (map tail xss)
+
+--trovo massimo e minimo di ogni colonna
+getMinMax :: [[Int]] -> [(Int,Int)]
+getMinMax = map (\col -> (minimum col, maximum col))
+
+--compongo le due funzioni con l'operatore di composizione "."
+colMinMax :: [[Int]] -> [(Int,Int)]
+colMinMax = getMinMax . extractCols 
+
+-----------------------------------------------------------------
+
+--9 scrivere una funzione che traspone una matrice (M memorizzata per righe)
+trasp:: [[Int]] -> [[Int]]
+trasp ([]:_) = []
+trasp xss = map head xss : trasp (map tail xss)
