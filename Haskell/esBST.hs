54f8e0070ab9f5d758a1b2c539480facfe47162c
riccardosantarossa <74408056+riccardosantarossa@users.noreply.github.com>
2025-07-02 17:59:33 +0200

    aggiunti inserimento e visita InOrder

--- Haskell/esBST.hs
+++ Haskell/esBST.hs
@@ -32,3 +32,28 @@ bstElem (Node lf v rt) x
 
 -----------------------------------------------------------------
 
+--5 inserimento di un valore in un BST
+insertBST :: Ord a => a -> Tree a -> Tree a
+insertBST x Nil = Node Nil x Nil
+insertBST x (Node lf v rt) 
+ | x == v = Node lf v rt
+ | x < v = Node (insertBST x lf) v rt
+ | x > v = Node lf v (insertBST x rt)
+
+-----------------------------------------------------------------
+
+--6 lista ordinata dei nodi di un BST (visita inOrder)
+bst2List :: Ord a => Tree a -> [a]
+bst2List Nil = []
+bst2List (Node lf v rt) =  bst2List lf ++ [v] ++ bst2List rt
+
+-----------------------------------------------------------------
+
+--7 ordinamento di liste mediante BST
+bstFromList :: Ord a => [a] -> Tree a
+bstFromList = foldr insertBST Nil 
+
+orderList :: Ord a => [a] -> [a]
+orderList xs = bst2List ( bstFromList xs )
+
+-----------------------------------------------------------------
